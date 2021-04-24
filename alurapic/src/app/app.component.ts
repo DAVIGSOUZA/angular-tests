592008a23f3b39a186c2a9deef76742631ef4acf
Davi Garcia <davigs@hotmail.com>
2021-04-23 22:58:09 -0300

    iniciando projeto

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic'
+  imgUrl = 'https://picsum.photos/200'
+}
---
+++ alurapic/src/app/responsiveImg/responsiveImg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-responsiveImg',
+    templateUrl: 'responsiveImg.component.html'
+})
+
+export class ResponsiveImgComponent {
+    imgUrl = 'https://picsum.photos/200'
+}
