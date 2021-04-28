1c272feada7189588b8188f05ab04fcfa9296899
Davi Garcia <davigs@hotmail.com>
2021-04-28 17:55:49 -0300

    continuando estudos

---
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'ap-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+  photos: Photo[] = []
+
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService.get().subscribe(photos => this.photos = photos)
+  }
+}
